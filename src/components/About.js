e4bae6cacc54f7134c7ea2f234ab7b32a1494bed
codericky <thevenom425@gmail.com>
2021-02-28 21:56:09 +0700

    edit card project

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,9 @@ const About = () => {
             <p class="text-black-600 text-center text-2xl font-bold mt-8">About</p>
             <div class="md:container md:mx-auto grid grid-rows-1 grid-flow-col mt-8">
   <div class="ml-8"><img src={img} alt="gambarku"/></div>
-  <div class="ml-8 mr-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquet euismod elit. Nullam ultricies accumsan dui, et dictum est gravida vel. Duis pellentesque malesuada lorem, quis dignissim risus ultrices ac. Nullam ante velit, ullamcorper at elit tincidunt, bibendum facilisis ante. In tortor est, scelerisque sit amet laoreet eget, maximus non quam. Nam scelerisque lacinia ex faucibus malesuada. Fusce vitae augue sit amet turpis elementum egestas sit amet non velit. Sed est purus, tristique sed pellentesque sed, blandit ac nulla. Nulla nec dui quis risus semper feugiat nec eget enim. Nullam fermentum lacus a erat egestas porttitor. Ut vel vestibulum nulla, id elementum neque. Aliquam erat volutpat. Proin at ultricies lacus, et efficitur ipsum. Donec eget lectus lacus. Donec et massa mollis, fermentum ex vel, ultrices enim. Aenean sollicitudin tempus consequat. </div>
+  <div class="ml-8 mr-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+   ultricies accumsan dui, et dictum est gravida vel. Duis pellentesque malesuada lorem, quis dignissim risus ultrices ac. Nullam ante velit, ullamcorper at elit tincidunt, bibendum facilisis ante. In tortor est, scelerisque sit amet laoreet eget, maximus non quam. Nam scelerisque lacinia ex faucibus malesuada. Fusce vitae augue sit amet turpis elementum egestas sit amet non velit. Sed est purus, tristique sed pellentesque sed, blandit ac nulla. Nulla nec dui quis risus semper feugiat nec eget enim. Nullam fermentum lacus a erat egestas porttitor. Ut vel vestibulum nulla, id elementum neque. Aliquam erat volutpat. Proin at ultricies lacus, et efficitur ipsum. 
+   ultrices enim. Aenean sollicitudin tempus consequat. </div>
 </div>
     </section>
     )
--- src/components/Project.js
+++ src/components/Project.js
@@ -5,16 +5,20 @@ const Project = () => {
         <section>
                 <p class="text-black-600 text-center text-2xl font-bold mt-8">Project</p>
                 <p class="text-center mt-5">Beberapa project yang pernah saya kerjakan</p>
-
-                <div class="h-64 grid grid-rows-1 grid-flow-col gap-4 mt-8">
+         <div class="h-64 grid grid-rows-1 grid-flow-col gap-4 mt-8">
   <p class="bg-green-500 work rounded-lg ml-8 text-center shadow-2xl">Work 1</p>
   <p class="bg-green-500 work rounded-lg mr-8 text-center shadow-2xl">Work 2</p>
-
-
- 
- 
- 
-</div>
+            </div>
+            <div class="h-64 grid grid-rows-1 grid-flow-col gap-4 mt-8">
+  <p class="bg-green-500 work rounded-lg ml-8 text-center shadow-2xl">Work 1</p>
+  <p class="bg-green-500 work rounded-lg mr-8 text-center shadow-2xl">Work 2</p>
+  <p class="bg-green-500 work rounded-lg  text-center shadow-2xl">Work 2</p>
+  <p class="bg-green-500 work rounded-lg mr-8 text-center shadow-2xl">Work 2</p>
+            </div>
+          <div class="h-64 grid grid-rows-1 grid-flow-col gap-4 mt-8">
+  <p class="bg-green-500 work rounded-lg ml-8 text-center shadow-2xl">Work 1</p>
+  <p class="bg-green-500 work rounded-lg mr-8 text-center shadow-2xl">Work 2</p>
+            </div>
         </section>
     )
 }
